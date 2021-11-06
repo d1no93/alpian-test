448193145dbf2e46fe218168e04fcc46c2846154
Shaaz <shaazabedin93@gmail.com>
2021-11-06 11:22:32 +0000

    Adding ability to create customer (through controller) + test.
    Adding ability to create customer (through controller) + test.

---
+++ src/main/java/com/shaazabedin/controller/CustomerMapperController.java
@@ -0,0 +1,31 @@
+package com.shaazabedin.controller;
+
+import com.shaazabedin.model.Customer;
+import com.shaazabedin.service.CustomerMappingService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Slf4j
+@RequestMapping(path = "/customerMapper")
+@RestController
+@EnableWebMvc
+public class CustomerMapperController {
+
+    private final CustomerMappingService customerMappingService;
+
+    public CustomerMapperController(CustomerMappingService customerMappingService) {
+        this.customerMappingService = customerMappingService;
+    }
+
+    @PostMapping(value="customer/{customerId}/createdAt/{createdAt}/create")
+    @ResponseBody
+        public ResponseEntity<String> createCustomer(@PathVariable(value = "customerId") String customerId,
+                                                     @PathVariable(value = "createdAt") String createdAt) {
+        this.customerMappingService.createCustomer(customerId, createdAt);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(String.format("Created Customer - Customer: %s, CreatedAt: %s", customerId, createdAt));
+    }
+}
---
+++ src/test/java/ex3/CustomerMapperControllerTest.java
@@ -0,0 +1,44 @@
+package ex3;
+
+import com.shaazabedin.Application;
+import com.shaazabedin.controller.CustomerMapperController;
+import com.shaazabedin.model.Customer;
+import com.shaazabedin.service.CustomerMappingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CustomerMapperController.class)
+@ContextConfiguration(classes={Application.class})
+public class CustomerMapperControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CustomerMappingService customerMappingService;
+
+    @Test
+    public void shouldSuccessfullyCreatedCustomer() throws Exception {
+        Customer customerMock = Mockito.mock(Customer.class);
+        when(customerMappingService.createCustomer("CustomerId", "2021-11-06"))
+                .thenReturn(customerMock);
+
+        this.mockMvc.perform(post("/customerMapper/customer/CustomerId/createdAt/2021-11-06/create"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Created Customer - Customer: CustomerId, CreatedAt: 2021-11-06"));
+    }
+}
